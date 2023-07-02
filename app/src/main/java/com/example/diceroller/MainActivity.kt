74546e821f5f31366d2645505c4703ac206345e4
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-07-02 15:59:25 -0300

    Tarea: Doble dado

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -10,11 +10,13 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
 
     lateinit var diceImage: ImageView
+    lateinit var diceImage2: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
 
         val rollButton = findViewById<Button>(R.id.roll_button)
 
@@ -55,12 +57,15 @@ class MainActivity : AppCompatActivity() {
      } */
 
     private fun rollDice() {
-        //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+    }
 
+    private fun getRandomDiceImage(): Int {
         val randomInt = (1..6).random()
-        diceImage = findViewById(R.id.dice_image)
 
-        val drawableResource = when (randomInt) {
+
+        return when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -68,8 +73,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-
-        diceImage.setImageResource(drawableResource)
     }
 
 }
